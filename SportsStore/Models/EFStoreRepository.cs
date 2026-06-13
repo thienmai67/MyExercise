c10dcf267b63dcd4f0c681b33ee7e7056789c226
thienmai67 <lehongcuc19571952@gmail.com>
2026-06-13 19:18:32 +0700

    Ok fen

---
+++ SportsStore/Models/EFStoreRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace SportsStore.Models
+{
+    public class EFStoreRepository : IStoreRepository
+    {
+        private StoreDbContext context;
+
+        public EFStoreRepository(StoreDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Product> Products
+            => context.Products;
+    }
+}
