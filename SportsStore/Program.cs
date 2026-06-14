3896a928674e377c313e96373055ac5c13ce7350
thienmai67 <lehongcuc19571952@gmail.com>
2026-06-14 15:30:35 +0700

    new_Lab03

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -7,21 +7,41 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<StoreDbContext>(opts =>
 {
-    opts.UseSqlServer(
-    builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
 
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.UseSession();
 
-app.MapControllerRoute("pagination",
-"Products/Page{productPage}",
-new { Controller = "Home", action = "Index" });
+app.MapControllerRoute(
+    name: "catpage",
+    pattern: "{category}/Page{productPage:int}",
+    defaults: new { Controller = "Home", action = "Index" });
+
+app.MapControllerRoute(
+    name: "page",
+    pattern: "Page{productPage:int}",
+    defaults: new { Controller = "Home", action = "Index", productPage = 1 });
+
+app.MapControllerRoute(
+    name: "category",
+    pattern: "{category}",
+    defaults: new { Controller = "Home", action = "Index", productPage = 1 });
+
+app.MapControllerRoute(
+    name: "pagination",
+    pattern: "Products/Page{productPage}",
+    defaults: new { Controller = "Home", action = "Index", productPage = 1 });
 
 app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 SeedData.EnsurePopulated(app);
 
